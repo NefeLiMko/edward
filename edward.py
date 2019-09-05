e319ee0e73549c1deae6bc6dbb385e0456544a8b
NefeLiMko <34207368+NefeLiMko@users.noreply.github.com>
2019-09-05 16:29:45 +0300

    Update edward.py

--- edward.py
+++ edward.py
@@ -1,15 +1,9 @@
 import sys
+import re
 
 filename = sys.argv[1]
 f = open(filename,'r')
 s = f.read()
-res = ''
-itr = enumerate(s)
-
-for ind, ch in itr:
-    if ind <len(s)-1 and ch == s[ind+1]:
-        next(itr)
-    else:
-        res += ch
-    
-print(res)
+s = re.sub(r"\n","",s)
+s = re.sub(r"(.)\1","",s)
+print(s)
